f2ce898045f0a9879d8e8bdb732f3a211fc898fd
Romulo R. Moreira <romulo.moreira@gmail.com>
2026-05-15 22:28:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Pytorch with Nvidia CUDA
+
+Initial project with Pytorch and Nvidia CUDA to start
+
+## 1. UV install
+Windows (Powershell):
+```shell
+irm https://astral.sh/uv/install.ps1 | iex
+```
+Linux/Mac:
+```shell
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
+
+## 2. Repository clone
+git clone https://github.com/romulorm/pytorch-gpu.git
+
+## 3. Syncronize project
+```shell
+cd pytorch-gpu
+uv sync
+```
+
+## 4. Activate virtual environment
+Windows:
+```shell
+.\.venv\Scripts\activate 
+```
+Linux/Mac:
+```shell
+source .venv/bin/activate
+```
+
+## 5. Execute main to test
+```shell
+python main.py
+```
+
+### ⚠️ If output shows Tensor matrix and device=CUDA, it's ready! 🚀
---
+++ main.py
@@ -0,0 +1,28 @@
+import torch
+
+def main():
+    
+
+    # Check if CUDA is available
+    cuda_available = torch.cuda.is_available()
+    print(f"CUDA Available: {cuda_available}")
+
+    if cuda_available:
+        # Print GPU details
+        print(f"GPU Name: {torch.cuda.get_device_name(0)}")
+        print(f"CUDA Version: {torch.version.cuda}")
+        print(f"PyTorch Version: {torch.__version__}")
+
+        # Perform a simple tensor calculation on the GPU
+        x = torch.rand(5, 3).cuda()
+        print("\nExample Tensor on GPU:")
+        print(30 * "-")
+        print("")
+        print(x)
+    else:
+        print("CUDA is not available. Check your drivers and toolkit.")
+
+
+
+if __name__ == "__main__":
+    main()
